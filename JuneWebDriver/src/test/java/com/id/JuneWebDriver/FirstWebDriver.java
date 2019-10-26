3117e8836e23c484f49eaac0a8b5ce90867d47c5
aulak <aulak@10.0.0.196>
2019-10-25 23:10:27 -0700

    1st commit

---
+++ JuneWebDriver/src/test/java/com/id/JuneWebDriver/FirstWebDriver.java
@@ -0,0 +1,16 @@
+package com.id.JuneWebDriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstWebDriver {
+public static void main(String[] args) {
+	
+	System.setProperty("webdriver.chrome.driver","C:\\Users\\aulak\\Desktop\\chromedriver_win32 (6)\\chromedriver.exe");
+	WebDriver driver=new ChromeDriver();
+	
+	driver.get("www.whiteboxqa.com");
+	driver.manage().window().maximize();
+}
+
+}

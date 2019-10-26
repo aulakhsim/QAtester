54b4415846950a4b54c55d4dba793d53f7870349
aulakhsim <aulakhsim@gmail.com>
2019-10-25 23:23:59 -0700

    1 commit

--- JuneWebDriver/src/test/java/com/id/JuneWebDriver/FirstWebDriver.java
+++ JuneWebDriver/src/test/java/com/id/JuneWebDriver/FirstWebDriver.java
@@ -9,7 +9,7 @@ public static void main(String[] args) {
 	System.setProperty("webdriver.chrome.driver","C:\\Users\\aulak\\Desktop\\chromedriver_win32 (6)\\chromedriver.exe");
 	WebDriver driver=new ChromeDriver();
 	
-	driver.get("www.google.com");
+	driver.get("www.yahoo.com");
 	driver.manage().window().maximize();
 }
 

3e5d6fc79d46720a100cd9adf2f2bb7f59c0667c
aulakhsim <aulakhsim@gmail.com>
2019-10-25 23:36:18 -0700

    1 commit in maven project

--- JuneWebDriver/src/test/java/com/id/JuneWebDriver/FirstWebDriver.java
+++ JuneWebDriver/src/test/java/com/id/JuneWebDriver/FirstWebDriver.java
@@ -9,7 +9,7 @@ public static void main(String[] args) {
 	System.setProperty("webdriver.chrome.driver","C:\\Users\\aulak\\Desktop\\chromedriver_win32 (6)\\chromedriver.exe");
 	WebDriver driver=new ChromeDriver();
 	
-	driver.get("www.yahoo.com");
+	driver.get("www.WellsFargo.com");
 	driver.manage().window().maximize();
 }
 

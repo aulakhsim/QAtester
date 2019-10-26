7c15f619c329a8ed0fdfa45eefbc2b4257395897
aulakhsim <aulakhsim@gmail.com>
2019-10-25 23:38:19 -0700

    again

--- JuneWebDriver/src/test/java/com/id/JuneWebDriver/FirstWebDriver.java
+++ JuneWebDriver/src/test/java/com/id/JuneWebDriver/FirstWebDriver.java
@@ -9,7 +9,7 @@ public static void main(String[] args) {
 	System.setProperty("webdriver.chrome.driver","C:\\Users\\aulak\\Desktop\\chromedriver_win32 (6)\\chromedriver.exe");
 	WebDriver driver=new ChromeDriver();
 	
-	driver.get("www.WellsFargo.com");
+	driver.get("www.BankOfAmerica.com");
 	driver.manage().window().maximize();
 }
 
